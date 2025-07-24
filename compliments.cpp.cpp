fff6caf75a3399059ee206e6b6fc6c5fd5c4a0b7
dhwanil-25 <thisisemma07@gmail.com>
2025-07-24 21:44:50 +0530

    Add files via upload
    getting rid of your insecurities isn't one day's work and i know this won't do much but hey, one step at a time i guess? this is really nothing but a random compliment generator for you ( wrote all the compliments (50) manually huh) yeah not really anything good or exciting just wanted to let you be known how much i adore you.

---
+++ compliments.cpp.cpp
@@ -0,0 +1,79 @@
+
+
+#include <iostream>
+#include <ctime>
+using std::string;
+#include <string>
+#include <vector>
+using std::cout;
+using std::cin;
+
+
+
+int main()
+{
+    string loop = "for(1==1){cout<<'ILOVEYOU' }";
+    srand(time(NULL));
+         int compliments = (rand() % 50) + 1;
+         switch (compliments) {
+         case 1: cout << "Hey beautiful <3" << '\n'; break;
+         case 2: cout << "YOU SMELL SO DAMN AMAZING" << '\n'; break;
+         case 3:cout << "You look so pretty baby" << '\n'; break;
+         case 4:cout << "It is literally unfair how hot you look rn" << '\n'; break;
+         case 5:cout << "I can't focus on my studies cause i keep thinking about that beautiful smile of yours <3 " << '\n'; break;
+         case 6:cout << "i forget everything everytime i look into your beautiful eyes." << '\n'; break;
+         case 7:cout << "i love that black tube top and especially that red dress on you, you look no lesser than a princess in them<3" << '\n'; break;
+         case 8:cout << "as much as i like that tube top and the red dress, i'd like it even better off you haha" << '\n'; break;
+         case 9:cout << "i'm so attracted to you" << '\n'; break;
+         case 10:cout << "i can't believe i lived 18 years without you, wtf was i doing?" << '\n'; break;
+         case 11:cout << "i want forever and ever with you" << '\n'; break;
+         case 12:cout << "you're my dream girl as fuck" << '\n'; break;
+         case 13:cout << "i love the way you smile on vc without saying anything." << '\n'; break; 
+         case 14:cout << "i'm so thankful for youuuuu" << '\n'; break;
+         case 15:cout << "YOU MAKE MY HEART HAPPY" << '\n'; break;
+         case 16:cout << "I LOVE YOUR BODY SO FUCKING MUCH" << '\n'; break;
+         case 17:cout << "THAT WAIST IS SO FUCKING HOT AND DELICIOUS" << '\n'; break;
+         case 18:cout << "SHORT HAIR LOOKS GREAT ON YOU, DONT GROW." << '\n'; break; 
+         case 19:cout << "I LOVE HOW YOU CARRY YOURSELF OUT THERE. SO DAMN ATTRACTIVE." << '\n'; break;
+         case 20:cout << "I LOVE YOUR LIPS SO MUCH THEY ARE SO GOOD BUT THEY ONLY LOOK THE BEST WHEN THEY ARE ON MINE HAHA" << '\n'; break;
+         case 21:cout << "YOUR NOSE IS SO BOOP BOOP CODED MWUAHDUAHSUHSUHSUH" << '\n'; break;
+         case 22:cout << "SWEATY FOREHEAD KISSES MWUAH MWUAH" << '\n'; break;
+         case 23:cout << "YOUR SELF CONFIDENCE IS SO ALPHA SO NICE I LOVE HIHIHIHIHIHI" << '\n'; break;
+         case 24:cout << "IF I WERE TO DESCRIBE IT IN CODING HOW MUCH I LOVE YOU, LET IT BE : " << '\n' << loop;
+             
+             break;
+         case 25:cout << "YOUR PRESENCE IS HEARTWARMING" << '\n'; break;
+         case 26:cout << "YOU ARE MY SAFE SPACE, MY HOME, MY COMFORT ZONE" << '\n'; break;
+         case 27:cout << "MY PRETTIESTTTTTTT BABYYYYYYYYYY" << '\n'; break;
+         case 28:cout << "I WANT TO KISS EVERY SQUARE INCH OF YOUR BODY" << '\n'; break; 
+         case 29:cout << "YOU MAKE ME THE HAPPIEST" << '\n'; break;
+         case 30:cout << "YOUR WAIST IS SO FUCKING PERFECT I LOVE IT I LOVE IT I LOVE IT SO MUCH " << '\n'; break; 
+         case 31:cout << "I LOVE YOUR EYES WHEN THEY STARE INTO MINE AND WE GET LOST INTO THE MOMENT" << '\n'; break;
+         case 32:cout << "I AM SO OBSESSED WITH YOU AND YOUR BEAUTY AND YOUR FIGUREEE ARGHHHHHHH" << '\n'; break;
+         case 33:cout << "I WANT TO BURY MY FACE IN YOUR BOOBS HAHAHJASAAFKAHFJSAK" << '\n'; break;
+         case 34:cout << "AINT A SINGLE DAY WHERE YOU LOOK BAD TF ( EVEN WHEN YOU DONT BATH )" << '\n'; break;
+         case 35:cout << "ARE YOU SALT? BECAUSE YOU ARE SODIUM FINEEEEE" << '\n'; break;
+         case 36:cout << "WIFE MATERIALLLLLLLL" << '\n'; break;
+         case 37:cout << "I LOVE THE WAY YOU COMFORT ME AND REASSURE ME<3" << '\n'; break;
+         case 38:cout << "THANK YOU FOR BEING WITH ME AND THANK YOU FOR EVERYTHING" << '\n'; break;
+         case 39:cout << "THANK YOU FOR NOT GIVING UP ON A LOSER LIKE ME" << '\n'; break;
+         case 40:cout << "YOU'RE THE MOST IMPORTANT PERSON TO MEEEE" << '\n'; break;
+         case 41:cout << "THE PRETTIEST GIRL GETS FEET RUB EVERY NIGHT SO COME OVER ASAP" << '\n'; break;
+         case 42:cout << "I LOVE WHEN WE KEPT KISSING AND CUDDLING FOR HOURS SO DAMN GOOD LITERALLY BEST MOMENT OF MY LIFE" << '\n'; break;
+         case 43:cout << "IMAGINE BEING INSECURE OF THIS PERFECT ASH BODY LMAO NO WONDER YOU'RE A DUMBASS" << '\n'; break;
+         case 44:cout << "YOU'RE IT. THAT GIRL." << '\n'; break;
+         case 45:cout << "YOU ARE SO PERFECT MANNNNNNN WAY OUTTA MY LEAGUE" << '\n'; break;
+         case 46:cout << "THERE'D BE NO MONA LISA AND ONLY ISHA GUPTA IF I WAS GOOD AT PAINTING." << '\n'; break;
+         case 47:cout << "YOUR BODY WARMTH MADE ALL MY OVERTHINKING AND PROBLEMS GO AWAY THAT DAY" << '\n'; break;
+         case 48:cout << "I'M HERE FOR YOU THROUGH ALL OF IT" << '\n'; break;
+         case 49:cout << "I'D RATHER HAVE YOU ON YOUR WORST DAY THAN ANYONE ELSE ON THEIR BEST." << '\n'; break;
+         case 50:cout << "YOU MAKE THE WORLD A BETTER PLACE." << '\n'; break;
+         
+           
+
+
+
+         }
+    return 0;
+}
+
